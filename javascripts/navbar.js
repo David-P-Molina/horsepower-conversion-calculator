6f19d47ff300502134e8d9041c36143309c0cd97
David Moina <davemolina777@gmail.com>
2021-06-11 13:21:23 -0500

    Added basic event listeners to links

--- javascripts/navbar.js
+++ javascripts/navbar.js
@@ -2,3 +2,8 @@ const standardToStrangeConversion = document.getElementById('standard-strange')
 const strangeToStrangeConversion = document.getElementById('strange-strange')
 const fullComparisonChart = document.getElementById('full-comparison-chart')
 const exampleConversion = document.getElementById('example-conversions')
+// fill out function that creates a form based on the selection
+standardToStrangeConversion.addEventListener('click', () => {})
+strangeToStrangeConversion.addEventListener('click', () => {})
+fullComparisonChart.addEventListener('click', () => {})
+exampleConversion.addEventListener('click', () => {})
