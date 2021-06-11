c05e47b43e7f8afa6c28adf1fa3c0226e8964e38
David Moina <davemolina777@gmail.com>
2021-06-11 13:17:14 -0500

    Added const event listeners for navbar links

---
+++ javascripts/navbar.js
@@ -0,0 +1,4 @@
+const standardToStrangeConversion = document.getElementById('standard-strange')
+const strangeToStrangeConversion = document.getElementById('strange-strange')
+const fullComparisonChart = document.getElementById('full-comparison-chart')
+const exampleConversion = document.getElementById('example-conversions')
