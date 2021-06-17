64b584cff478654c6a36a4ed36e39404db962bef
David Moina <davemolina777@gmail.com>
2021-06-17 13:38:15 -0500

    Added constructor method and base class structure

---
+++ javascripts/measurementClass.js
@@ -0,0 +1,9 @@
+class Measurement {
+    constructor(id, animal_power_to_hp, hp_equivalent, source, standard) {
+        this.id = id;
+        this.animal_power_to_hp = animal_power_to_hp;
+        this.hp_equivalent = hp_equivalent;
+        this.source = source;
+        this.standard = standard
+    }
+}
