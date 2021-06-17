42361bb6165439b9d0314eaef32616c91c2a98e3
David Moina <davemolina777@gmail.com>
2021-06-17 13:43:09 -0500

    Added user_id to constructor method

--- javascripts/conversionClass.js
+++ javascripts/conversionClass.js
@@ -1,8 +1,9 @@
 class Conversion {
-    constructor(id, name, measure_1_quantity, measure_2_quantity) {
+    constructor(id, name, measure_1_quantity, measure_2_quantity, user_id) {
         this.id = id;
         this.name = name;
         this.measure_1_quantity = measure_1_quantity;
         this.measure_2_quantity = measure_2_quantity;
+        this.user_id = user_id;
     }
 }
