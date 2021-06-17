ba6cfdc9db47facbc1a65a71b96973903fa751dc
David Moina <davemolina777@gmail.com>
2021-06-17 13:41:21 -0500

    Added basic convresion class & constructor method

---
+++ javascripts/conversionClass.js
@@ -0,0 +1,8 @@
+class Conversion {
+    constructor(id, name, measure_1_quantity, measure_2_quantity) {
+        this.id = id;
+        this.name = name;
+        this.measure_1_quantity = measure_1_quantity;
+        this.measure_2_quantity = measure_2_quantity;
+    }
+}
