9b5db50c9799b8335be62aff1533c33036b24761
David Moina <davemolina777@gmail.com>
2021-06-17 12:35:54 -0500

    Added calculate hpvalue function and psuedocode purpose

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -3,3 +3,12 @@ const hpInput = document.getElementById('horsepower-number-input')
 const table = document.getElementsByTagName('table')
 
 hpInput.addEventListener('input', updateAnimalPowerValue);
+let animals = []
+const updateAnimalPowerValue = (e) => {
+    animals.calculateHpValue = // eventlistener
+}
+
+const calculateHpValue = (array) => {
+    array.map // takes each animal object and multiply the input by the animals hp equivalent 
+    // & sets table column value to that  
+}
