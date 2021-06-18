a4e7bc644ddbd7da63f302666b45ce762dfcf577
David Moina <davemolina777@gmail.com>
2021-06-18 11:47:15 -0500

    updated conversoion attribute names

--- javascripts/conversionClass.js
+++ javascripts/conversionClass.js
@@ -1,9 +1,9 @@
 class Conversion {
-    constructor(id, name, measure_1_quantity, measure_2_quantity, user_id) {
+    constructor(id, name, hp_quantity, animal_conversion_quantity, user_id) {
         this.id = id;
         this.name = name;
-        this.measure_1_quantity = measure_1_quantity;
-        this.measure_2_quantity = measure_2_quantity;
+        this.hp_quantity = hp_quantity;
+        this.animal_conversion_quantity = animal_conversion_quantity;
         this.user_id = user_id;
     }
 }
