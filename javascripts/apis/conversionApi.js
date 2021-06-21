d553286756b05b65386016b6eea3558e9029f7d3
David Moina <davemolina777@gmail.com>
2021-06-21 11:36:26 -0500

    Added basic fetch for Conversion Api

--- javascripts/apis/conversionApi.js
+++ javascripts/apis/conversionApi.js
@@ -1,3 +1,10 @@
 class ConversionApi {
     static url = `${baseUrl}/conversions`
+
+    static fetchConversions() {
+        fetch(`${url}/conversions`)
+        .then(response => response.json())
+        .then(json => renderConversions(json))
+    }
+    
 }
