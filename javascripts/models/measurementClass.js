69f5708a035ecac655f55123238e61814a338b5b
David Moina <davemolina777@gmail.com>
2021-06-23 17:27:25 -0500

    Added basic source list functionality

--- javascripts/models/measurementClass.js
+++ javascripts/models/measurementClass.js
@@ -1,6 +1,6 @@
 class Measurement {
     static all = [];
-    
+    static sourceList = [];
     constructor({id, name, animal_power_to_hp, hp_equivalent, source, standard}) {
         this.id = id;
         this.name = name;
@@ -22,7 +22,14 @@ class Measurement {
         this.findByName(measureObj.name) || new Measurement(measureObj)
     }
     static handleSourceList(e) {
-        //
+        const listLine = document.createElement('h3')
+        const sourceLink = document.createElement('a')
+        listLine.id = `${this.name}-header`
+        sourceLink.href = this.source
+        sourceLink.id =`${this.name}-power-source`
+        sourceLink.innerHTML = `Website Link for ${this.name}`
+        listLine.appendChild(sourceLink)
+        mainDisplay.appendChild(listLine)
     }
     
 }
