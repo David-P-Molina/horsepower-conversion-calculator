54db9c971d41b51f711fe86edddad7eeff1f803d
David Moina <davemolina777@gmail.com>
2021-06-23 15:40:24 -0500

    fixed typos

--- javascripts/models/measurementClass.js
+++ javascripts/models/measurementClass.js
@@ -1,7 +1,7 @@
 class Measurement {
     static all = [];
-
-    constructor({id, name. animal_power_to_hp, hp_equivalent, source, standard}) {
+    
+    constructor({id, name, animal_power_to_hp, hp_equivalent, source, standard}) {
         this.id = id;
         this.name = name;
         this.animal_power_to_hp = animal_power_to_hp;
